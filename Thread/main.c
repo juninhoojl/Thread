fe6e4d49f4daf026a5662590e7155576b3c5742f
José Luiz Corrêa Júnior <juninhopc@icloud.com>
2018-09-04 17:33:05 -0300

    Initial Commit

---
+++ Thread/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Thread
+//
+//  Created by José Luiz Junior on 04/09/18.
+//  Copyright © 2018 José Luiz Junior. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
