6d04a9d32fb61dde51233230f86071cd4bc4951b
José Luiz Corrêa Júnior <juninhopc@icloud.com>
2018-09-04 18:42:42 -0300

    finalizado

--- Thread/main.c
+++ Thread/main.c
@@ -10,46 +10,48 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-#define N_THREAD 4
+#define QTD_Thread 8//Quantidade de threads a ser usada
 
-void delay(){
+void tempo(int laco){//so para gastar um pouquinho de tempo tempo
+    int a, b, c;
     
-    int i, j, k, valor = 1000;
-    
-    for (i = 0; i < valor; i++)
-        for (j = 0; j < valor; j++)
-            for (k = 0; k < valor; k++)
-                valor = valor;
-    
+    for (a = 0; a < laco; a++)
+        for (b = 0; b < laco; b++)
+            for (c = 0; c < laco; c++)
+                laco = laco;
+  
 }
 
 
-void funcao(void *arg){
-    int *pvalor;
+void *funcao(void *thread_id){
+    int *ident;
+
+    ident = thread_id;
     
-    pvalor = arg;
+    printf("• Thread %i\n", *ident);//alerta inicio
+    tempo(1000);
+    printf("o Thread %i\n", *ident);//alerta fim
     
-    printf("Thread %i executando... \n", *pvalor);
-    delay();
-    printf("Thread %i executando... \n", *pvalor);
-    
+    return NULL;//Para evitar bug no XCODE
 }
 
 
 int main(int argc, const char * argv[]) {
-    pthread_t threads[N_THREAD];//criamos um vetor com a quantidade
-    int i, arg[N_THREAD];
+    pthread_t threads[QTD_Thread];//criamos um vetor de thread usando o TAD phtread_t
+    int i, qtd[QTD_Thread];
     
-    for (i = 0; i < N_THREAD; i++) {
-        arg[i] = i + 1;
-        pthread_create(&threads[i], NULL, funcao, (void *)&arg[i]);
+    for (i = 0; i < QTD_Thread; i++) {
+        qtd[i] = i + 1;
+        pthread_create(&threads[i], NULL, funcao, (void *)&qtd[i]);//funcao para criacao das threads
+        //Endereco de memoria da thread/ parametro nulo/ funcao que ela vai executar/ e o argumento da funcao
         
     }
     
-    for (i = 0; i < N_THREAD; i++) {
+    //laco para esperar as threads retornarem para depois seguir em frente
+    for (i = 0; i < QTD_Thread; i++) {
         pthread_join(threads[i], NULL);
     }
-    printf("/n");
+    printf("\n");
     
     return 0;
 }
